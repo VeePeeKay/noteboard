71b2681454ff3402465b223695a5bec2bbc8dad8
richkats <katsur.serg@gmail.com>
2020-12-30 04:47:20 +0500

    model and view update

--- view.py
+++ view.py
@@ -3,10 +3,13 @@
 import sys
 from PyQt5 import QtWidgets, uic
 from model import *
+import logging
 from time import sleep
 
 SignupForm, _ = uic.loadUiType("registration.ui")
 WelcomeForm, _ = uic.loadUiType("welcum.ui")
+MainForm, _ = uic.loadUiType("main.ui")
+
 
 def alert(msg):
     error_dialog = QtWidgets.QMessageBox()
@@ -17,8 +20,14 @@ def alert(msg):
 class WelcomeUi(QtWidgets.QDialog, WelcomeForm):
     def __init__(self):
         super(WelcomeUi, self).__init__()
+        self.signup = SignupUi()
         self.setupUi(self)
         self.loginButt.clicked.connect(self.login)
+        self.registrButt.clicked.connect(self.openWindowSignup)
+
+    def openWindowSignup(self):
+        #self.close()
+        self.signup.exec()
 
     def login(self):
         db = DB()
@@ -26,21 +35,23 @@ class WelcomeUi(QtWidgets.QDialog, WelcomeForm):
         passw = self.passEdit.text()
         exist = db.userExist(login, passw)
         if exist:
-            user = User(login)
-            print(user)
+            self.user = User(login)
+            self.mainwindow = MainUi(self.user)
+            self.close()
+            self.mainwindow.exec()
         else:
-            error_dialog = QtWidgets.QMessageBox()
-            error_dialog.setText('GOVNO LOGIN, PEREDELAY!')
-            error_dialog.exec_()
+            alert('GOVNO LOGIN, PEREDELAY!')
 
 
 class SignupUi(QtWidgets.QDialog, SignupForm):
     def __init__(self):
         super(SignupUi, self).__init__()
         self.setupUi(self)
+        #self.login = WelcomeUi()
         self.finishRegButt.clicked.connect(self.signup)
 
     def signup(self):
+        print("signup")
         db = DB()
         login = self.createLogEdit.text()
         passw = self.createPassEdit.text()
@@ -51,13 +62,19 @@ class SignupUi(QtWidgets.QDialog, SignupForm):
         else:
             user = User(login, passw)
             alert('WELCUM TO THE CLUB, BUDDY (EST PROBITIE)')
+            self.close()
 
+
+class MainUi(QtWidgets.QDialog, MainForm):
+    def __init__(self, user):
+        super(MainUi, self).__init__()
+        self.setupUi(self)
 
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    w = SignupUi()
+    w = WelcomeUi()
     w.show()
     sys.exit(app.exec_())
