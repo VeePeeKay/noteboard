0d9867d7d31246bd25c0c7d04b551b1a5c7a8772
richkats <katsur.serg@gmail.com>
2020-12-29 20:44:31 +0500

    Edited view.py

--- view.py
+++ view.py
@@ -1,10 +1,30 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PySide2.QtWidgets import QApplication, QLabel
+from PyQt5 import QtWidgets, uic
+from model import *
+
+Form, _ = uic.loadUiType("welcum.ui")
 
 
-app = QApplication(sys.argv)
-label = QLabel("Hello World!")
-label.show()
-app.exec_()
+class Ui(QtWidgets.QDialog, Form):
+    def __init__(self):
+        super(Ui, self).__init__()
+        self.setupUi(self)
+        self.loginButt.clicked.connect(self.login)
+
+    def login(self):
+        db = DB()
+        login = self.signLog.toPlainText()
+        print(login)
+        exist = bool(db.select("users", "name", login))
+        print(exist)
+        if exist:
+            user = User(login)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    w = Ui()
+    w.show()
+    sys.exit(app.exec_())
